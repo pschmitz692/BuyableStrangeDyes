0364aa817188034e948167031b0fded39c9083c7
Parker Schmitz <pschmitz@iastate.edu>
2024-01-09 03:56:20 -0600

    Initial commit. Dye Trader sells all Strange Dyes once Hardmode activates.

---
+++ BuyableStrangeDyes.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace BuyableStrangeDyes
+{
+	public class BuyableStrangeDyes : Mod
+	{
+	}
+}
---
+++ NPC/StrangeDyeTrader.cs
@@ -0,0 +1,54 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BuyableStrangeDyes.NPC
+{
+    internal class StrangeDyeTrader : GlobalNPC
+    {
+        static short[] strangeDyes = {
+            ItemID.AcidDye,
+            ItemID.BlueAcidDye,
+            ItemID.RedAcidDye,
+            ItemID.ChlorophyteDye,
+            ItemID.GelDye,
+            ItemID.MushroomDye,
+            ItemID.GrimDye,
+            ItemID.HadesDye,
+            ItemID.BurningHadesDye,
+            ItemID.ShadowflameHadesDye,
+            ItemID.LivingOceanDye,
+            ItemID.LivingFlameDye,
+            ItemID.LivingRainbowDye,
+            ItemID.MartianArmorDye,
+            ItemID.MidnightRainbowDye,
+            ItemID.MirageDye,
+            ItemID.NegativeDye,
+            ItemID.PixieDye,
+            ItemID.PhaseDye,
+            ItemID.PurpleOozeDye,
+            ItemID.ReflectiveDye,
+            ItemID.ReflectiveCopperDye,
+            ItemID.ReflectiveGoldDye,
+            ItemID.ReflectiveObsidianDye,
+            ItemID.ReflectiveMetalDye,
+            ItemID.ReflectiveSilverDye,
+            ItemID.ShadowDye,
+            ItemID.ShiftingSandsDye,
+            ItemID.DevDye,
+            ItemID.TwilightDye,
+            ItemID.WispDye,
+            ItemID.InfernalWispDye,
+            ItemID.UnicornWispDye
+                };
+
+        public override void ModifyShop(NPCShop shop)
+        {
+            if(shop.NpcType == NPCID.DyeTrader) {
+                foreach(short itemid in strangeDyes) { 
+                    shop.Add(new Item(itemid) { shopCustomPrice = 1000000 }, Condition.Hardmode);
+                }
+            }
+        }
+    }
+}
